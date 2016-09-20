515bbb32c0cbef4635e27fc9055f9ec40e4f18fd
Barry Ceelen <barry@multipop.org>
2016-09-20 13:14:02 +0200

    Move $anonymize_ip

--- wp-no-cookie-analytics.php
+++ wp-no-cookie-analytics.php
@@ -50,14 +50,6 @@ function analytics_without_cookies_enqueue_script() {
 	 */
 	$tracking_id = apply_filters( 'analytics_without_cookies_tracking_id', '' );
 
-	/**
-	 * Anonymize IP?
-	 *
-	 * @since 1.0.0
-	 */
-	$anonymize_ip = apply_filters( 'analytics_without_cookies_anonymize_ip', true );
-	$anonymize_ip = $anonymize_ip ? "ga('set', 'anonymizeIp', true);" : '';
-
 	if ( ! empty( $tracking_id ) ) {
 
 		$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.min' : '';
@@ -69,6 +61,14 @@ function analytics_without_cookies_enqueue_script() {
 			'1.4.1',
 			true
 		);
+
+		/**
+		 * Anonymize IP?
+		 *
+		 * @since 1.0.0
+		 */
+		$anonymize_ip = apply_filters( 'analytics_without_cookies_anonymize_ip', true );
+		$anonymize_ip = $anonymize_ip ? "ga('set', 'anonymizeIp', true);" : '';
 
 		$script = sprintf(
 			"var fp = new Fingerprint2();fp.get(function(result) {(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '%s', {'storage': 'none','clientId': result});%sga('send', 'pageview');});",
